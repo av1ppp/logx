b19cbd3386681cce99e8fd4fc419394f8d179645
Avi Parampampam <aviparampampam@gmail.com>
2024-12-28 13:56:27 +0500

    minor updatte

--- demo/main.go
+++ demo/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
+	"context"
 	"os"
-	"time"
-
-	"github.com/fatih/color"
 
 	"github.com/av1ppp/logx"
-	"github.com/av1ppp/logx/handlercolor1"
+	"github.com/av1ppp/logx/handlercolor2"
 )
 
 func main() {
@@ -17,22 +15,35 @@ func main() {
 }
 
 func innerMain() error {
-	logger := logx.New(handlercolor1.New(os.Stderr, &handlercolor1.Options{
-		Level:         logx.LevelDebug,
-		TimeFormat:    time.DateTime,
-		SrcFileMode:   handlercolor1.ShortFile,
-		SrcFileLength: 0,
-		MsgPrefix:     color.HiWhiteString("| "),
-		MsgLength:     0,
-		MsgColor:      color.New(),
-		NoColor:       false,
+	logger := logx.New(handlercolor2.New(os.Stdout, &handlercolor2.Options{
+		Level: logx.LevelDebug,
 	}))
 
-	logger.Debug("debug message")
-	logger.Verbose("verbose message")
-	logger.Info("info message")
-	logger.Warn("warn message")
-	logger.Error("error message")
+	// logger := logx.New(handlercolor1.New(os.Stderr, &handlercolor1.Options{
+	// 	Level:         logx.LevelDebug,
+	// 	TimeFormat:    time.DateTime,
+	// 	SrcFileMode:   handlercolor1.ShortFile,
+	// 	SrcFileLength: 0,
+	// 	MsgPrefix:     color.HiWhiteString("| "),
+	// 	MsgLength:     0,
+	// 	MsgColor:      color.New(),
+	// 	NoColor:       false,
+	// }))
+
+	logger.Debug("debug message 1")
+	logger.Log(context.Background(), logx.LevelDebug+1, "debug message 2")
+
+	logger.Verbose("verbose message 1")
+	logger.Log(context.Background(), logx.LevelVerbose+1, "verbose message 2")
+
+	logger.Info("info message 1")
+	logger.Log(context.Background(), logx.LevelInfo+1, "verbose info 2")
+
+	logger.Warn("warn message 1")
+	logger.Log(context.Background(), logx.LevelWarn+1, "warn info 2")
+
+	logger.Error("error message 1")
+	logger.Log(context.Background(), logx.LevelError+1, "error info 2")
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,10 @@ go 1.23.3
 require (
 	github.com/MatusOllah/slogcolor v1.4.0
 	github.com/av1ppp/timex v0.0.0-20241123002339-0bfb0edfb188
-	github.com/fatih/color v1.18.0
 )
 
 require (
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.27.0 // indirect
