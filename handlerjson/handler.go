984b90cac4289d682405dc12eb9d723960eb6edc
Avi Parampampam <aviparampampam@gmail.com>
2025-01-09 21:10:53 +0500

    minor update

--- handlerjson/handler.go
+++ handlerjson/handler.go
@@ -2,6 +2,8 @@ package handlerjson
 
 import "log/slog"
 
+type Options = slog.HandlerOptions
+
 // New creates a [JSONHandler] that writes to w,
 // using the given options. If opts is nil, the default options are used.
 var New = slog.NewJSONHandler
--- handlertext/handler.go
+++ handlertext/handler.go
@@ -2,6 +2,8 @@ package handlertext
 
 import "log/slog"
 
+type Options = slog.HandlerOptions
+
 // New creates a [TextHandler] that writes to w,
 // using the given options.
 // If opts is nil, the default options are used.
