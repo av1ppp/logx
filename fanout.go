659d0fad6e72f9f8d7d4f3147e68d8c007ed6302
Avi Parampampam <aviparampampam@gmail.com>
2025-01-09 21:25:27 +0500

    minor update

--- fanout.go
+++
@@ -1,8 +0,0 @@
-package logx
-
-import multi "github.com/samber/slog-multi"
-
-// Fanout distributes records to multiple [Handler] in parallel
-func Fanout(handlers ...Handler) Handler {
-	return multi.Fanout(handlers...)
-}
--- handler.go
+++ handler.go
@@ -1,5 +1,14 @@
 package logx
 
-import "log/slog"
+import (
+	"log/slog"
+
+	multi "github.com/samber/slog-multi"
+)
 
 type Handler = slog.Handler
+
+// JoinHandlers creates a Handler that writes to all handlers in the given list.
+func JoinHandlers(handlers ...Handler) Handler {
+	return multi.Fanout(handlers...)
+}
